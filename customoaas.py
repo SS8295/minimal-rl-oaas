3c4eeef9351eaa0e222321b0c1654d365373960a
SS8295 <stelios.stavroulakis1@zebra.com>
2022-07-27 00:34:16 -0700

    implemented progress_time, accept_new_tasks, accepted_new_employees

--- customoaas.py
+++ customoaas.py
@@ -1,5 +1,7 @@
 # Gym-related imports
 from math import floor
+from venv import create
+
 from gym import Env
 from gym import logger, spaces
 #from gym.spaces import Discrete, Box
@@ -8,174 +10,272 @@ from gym import logger, spaces
 import sys
 import time 
 import random
+import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+import cv2
+import numpy as np
+import glob
+from datetime import datetime
+import dataframe_image as dfi
 
 # Local imports
 from libr import *
-from libr_rl import *
+from libr_data import *
 from libr_floorplan import *
+from libr_nns import *
+from libr_plot import *
 
 class OaasEnv(Env):
 
-    def __init__(self, floorplan):
+    def __init__(self, floorplan, all_time_employees, all_time_tasks):
         
-        # Grid Size
+        # Database Parameters
+        self.employee_list, self.task_list = fetch_all_daily_employees_tasks(all_time_employees, all_time_tasks)
+        self.employee_df = create_employee_df(self.employee_list)
+        self.task_df = create_task_df(self.task_list)
+
+        # Grid Configuration
         self.floorplan = floorplan
-        #self.floorplan = floorplan
-        self.grid_size = len(floorplan) #len(floorplan)
-        self.allowed_floorplan = allowed_floorplan(floorplan)
-        #print(self.allowed_floorplan)
-        #sys.exit(0)
-        #Agents in Environment
-        self.num_agents = 3
-        self.num_tasks = 1
-        # Action Enumeration
-        self.num_actions = 5
-
-        # Employee Summoning
-        self.agent_dict = dict()
-        for player_int in range(self.num_agents):
-            self.agent_dict['player'+str(player_int)] = Employee(self.grid_size, self.allowed_floorplan)
-            #print(self.agent_dict['player'+str(player_int)].coords)
-        #sys.exit(0)
-        for key, value in self.agent_dict.items() :
-            print(key, value)
-        #sys.exit(0)
-
-        # Task Summoning
-        self.task_dict = dict()
-        self.task_dict['task1'] = Task(self.grid_size, self.allowed_floorplan)
+        self.grid_size = len(floorplan)
+        self.allowed_floorplan = allowed_floorplan(floorplan)      
+ 
+        # Numerical Initializations
+        self.num_employees = len(self.employee_list)
+        self.num_tasks = len(self.task_list)
+        
+        self.curr_assignments = []
+        self.finished_tasks = []
 
         # Observation and Action Space
-        low = np.zeros(2*(self.num_agents))
-        high = np.full(2*(self.num_agents), self.grid_size)
-        self.action_space = []
-        self.observation_space = []
-        for _ in range(self.num_agents):
-            self.action_space.append(spaces.Discrete(5))
-            self.observation_space.append(spaces.Box(np.float32(low), np.float32(high)))
-        print("Observation Space = ", self.observation_space)
-        print("Action Space = ",self.action_space)
+        #low = np.zeros(4*(self.num_employees))
+        #high = np.full(4*(self.num_employees), self.grid_size)
 
-    def step(self, action_list):
+        #self.action_space = spaces.Discrete(5)
+        #self.observation_space = spaces.Box(np.float32(low), np.float32(high))
+        #print("Observation Space = ", self.observation_space)
+        #print("Action Space = ",self.action_space)
 
-        self.done_flag = False
+    def progress_time(self, curr_time, curr_employees):
+        '''
+        Takes the current (passed) time and the head count of the store and updates the status
+        of the tasks based on the confluence diagram.
+
+        Inputs:
+                - Current time
+                - Current Employees
+        Ouput:
+                - Changes internally the self.employee_df and self.task_df variables accordingly 
         
-        # Move agents in environment
-        for act in range(len(action_list)):
-            self.agent_dict['player'+str(act)].move(action_list[act])
+        '''
 
-        reward_list = [0,0,0]
-        #aux_rew = self.aux_reward()*self.aux_multiplier
-        #print('Aux Reward = ',-aux_rew)
-        #reward_list = [-aux_rew, -aux_rew]
+        # The following changes the self.employee_df and self.task_df based on the self.curr_assignments
+        for assignment in self.curr_assignments:
 
-        if self.team_wins() and self.adv_wins():
-            reward_list = [0,0,0]
-            self.done_flag = True
+            employee_present_flag = False
+            finish_threshold = False
 
-        if self.team_wins():
-            reward_list = [1,1,-1]
-            self.done_flag = True
+            # Preproceess employee and time elapsed flag
+
+            if assignment['TIME_ASSIGNED'] + timedelta(minutes = assignment['TASK_DURATION'].item()) < curr_time:
+                finish_threshold = True
+
+            for employee in curr_employees:
+                #print(assignment['AgentID'])
+                #sys.exit(0)
+                #print(employee)
+                #sys.exit(0)
+                if assignment['AgentID'] in curr_employees['AgentID']:
+                    employee_present_flag = True
+            
+            # Change status of task if employee left without finishing
+            # TASK:         PROCESSING -> READY
+            # TASK:         PROCESSING -> MISSED
+            # EMPLOYEE:     BUSY -> -    
+            if not employee_present_flag and not finish_threshold:
+                print('Employee ', assignment['AgentID'], 'left without finihsing the task!')
+                self.curr_assignments.remove(assignment)
+                self.task_df.loc[self.task_df['UUID']==assignment.get('UUID'),'Status'] = 'READY'
+
+            # If time elapsed, mask task completed. For now, it does not matter if the employee left or not (t < 15min)
+            elif finish_threshold:
+
+                print('Task ', assignment['UUID'], ' finished!')
+
+                self.finished_tasks.append({'UUID':assignment['UUID'], 'finished_time':curr_time, 'employee':1})
+                self.curr_assignments.remove(assignment)
+
+                self.task_df.loc[self.task_df['UUID']==assignment.get('UUID'),'Status'] = 'FINISHED'
+                self.employee_df.loc[self.employee_df['AgentID']==assignment.get('AgentID'),'Status'] = 'READY'
+
+            # Task still in progress, no change in the status of the employee and task.    
+            else:
+                continue
+
+    def accept_new_tasks(self, curr_tasks):
+        '''
+        This function populates the self.task_df with tasks and changes their status to READY
+        while also incorporating the metadata from the task manager.
+        '''
+
+        for curr_task in range(len(curr_tasks)):
+            
+            if self.task_df.loc[self.task_df['UUID']==curr_tasks.loc[curr_task].get('UUID')]['Status'].item() == 'OUT':
+                self.task_df.loc[self.task_df['UUID']==curr_tasks.loc[curr_task].get('UUID'),'Status'] = 'READY'
+
+                for metadata in curr_tasks:
+                    self.task_df.loc[self.task_df['UUID']==curr_tasks.loc[curr_task].get('UUID'),metadata] = curr_tasks.loc[curr_task].get(metadata)
+
+    def accept_new_employees(self, curr_employees):
+
+        '''
+        This function populates the self.employee_df with employees and changes status of fresh 
+        new employees that haven't been assigned a task to READY and to employees that left the store 
+        to OUT while also incorporating the metadata from the task manager.
+        '''
+
+        self.employee_df = create_employee_df(self.employee_list)
+        self.employee_df['Location'] = self.employee_df['Location'].astype('object')
+
+        for curr_employee in range(len(curr_employees)):
+            
+            curr_employee_id = curr_employees.loc[curr_employee].get('AgentID')
+            
+            if self.employee_df.loc[self.employee_df['AgentID']==curr_employee_id]['agent_state'].item() == 'OUT':
+                self.employee_df.loc[self.employee_df['AgentID']==curr_employee_id,'agent_state'] = 'READY'
+                self.employee_df.loc[self.employee_df['AgentID']==curr_employee_id,'Location'] = [curr_employees.loc[curr_employee,'Location']]
+                self.employee_df.loc[self.employee_df['AgentID']==curr_employee_id,'Skillset'] = 'ALL'
+
+    def step(self, action, sched_int, curr_time, curr_tasks):
+
+        for assignment in action:
+            new_task_flag = True
+
+            for prev_assignments in self.curr_assignments:
+                if assignment['UUID'] == prev_assignments['UUID']:
+                    new_task_flag = False
+            
+            if new_task_flag == False:
+                print("attempting to do a task that is already being done")
+            else:
+                
+                self.curr_assignments.append({'UUID': assignment['UUID'], 'AgentID': assignment['AgentID'], 'TIME_ASSIGNED':curr_time,
+                                                'TASK_DURATION':curr_tasks.loc[curr_tasks['UUID']==assignment['UUID'],'TASK_DURATION']})
+
+                self.task_df.loc[self.task_df['UUID']==assignment.get('UUID'),'Status'] = 'PROCESSING'
+                self.employee_df.loc[self.employee_df['AgentID']==assignment.get('AgentID'),'Status'] = 'BUSY'
+                print("Scheduled a new task ", assignment['UUID'],' to ', assignment['AgentID'])
+                print(self.task_df.loc[self.task_df['UUID']==assignment.get('UUID')])
+                print(self.employee_df.loc[self.employee_df['AgentID']==assignment.get('AgentID')])
+                print("Assingment history = ",self.curr_assignments)            
         
-        if self.adv_wins():
-            reward_list = [-1,-1,1]
-            self.done_flag = True
-        
-        return self.obs(), reward_list, self.done_flag, {}
+
+        new_env_state = []
+        new_env_state.append(self.employee_df)
+        new_env_state.append(self.task_df)
+
+        reward = 0
+        done = False
+        info = {}
+
+        return new_env_state, reward, done, info
+
+    def matching_step(self, matching):
+        horizon = 30
+
+        for instance in range(horizon):
+            print('Step ', instance)
+            self.render(instance)
+            for match in matching:
+                
+                if len(match[0]) <= len(match[1]):
+                    employee = match[0]
+                    task = match[1]
+                else:
+                    employee = match[1]
+                    task = match[0]
+                #print('employee: ',employee)
+                #print('task: ',task)
+                #print(self.employee_dict[employee].coords, self.task_dict[task].coords)
+                path = astar(self.floorplan, self.employee_dict[employee].coords, self.task_dict[task].coords)
+                #print(path)
+                if len(path) > 0:
+                    self.employee_dict[employee].coords = path[-1]
+                else:
+                    pass
 
     def render(self,iter):
-        print("I will render")
-        fig,ax = plt.subplots(1)
+        #print("rendering...")
+        fig,ax = plt.subplots(1,2)
+
         fig.subplots_adjust(bottom=0.4)
         colorstate = np.zeros((self.grid_size, self.grid_size,3),dtype=np.uint8)
         colorstate = color_walls(colorstate, self.allowed_floorplan)
 
-        # Render Agents      
-        for player_int in range(self.num_agents):
-            colorstate[self.agent_dict['player'+str(player_int)].x][self.agent_dict['player'+str(player_int)].y] = (150, 55, 255)
+        # Render Employees      
+        for employee_int in range(self.num_employees):
+            colorstate[self.employee_dict[self.db_employees['AgentID'][employee_int]].coords[0]][self.employee_dict[self.db_employees['AgentID'][employee_int]].coords[1]] = (150, 55, 255)
 
         # Render Tasks      
-        colorstate[self.task_dict['task1'].x][self.task_dict['task1'].y] = (255, 165, 0)
-        #colorstate[self.agent_dict['player'+str(self.num_agents)].x][self.agent_dict['player'+str(self.num_agents)].y] = (255, 50, 50)
+        for task_int in range(self.num_tasks):
+            colorstate[self.task_dict[self.db_tasks['UUID'][task_int]].coords[0]][self.task_dict[self.db_tasks['UUID'][task_int]].coords[1]] = self.task_dict[self.db_tasks['UUID'][task_int]].color
+
+        # Render Tasks 
+        #if self.task_dict      
+        #colorstate[self.task_dict['task1'].coords[0]][self.task_dict['task1'].coords[1]] = (255, 165, 0)
         ax.imshow(colorstate, interpolation='None')
         ax.axis('off')
 
-        for player in range(self.num_agents):
-            x_coord = self.agent_dict['player'+str(player)].x
-            y_coord = self.agent_dict['player'+str(player)].y
-            dist = astar(self.floorplan, self.agent_dict['player'+str(player)].coords, self.task_dict['task1'].coords)
-            fig.text(0.3,0.3-0.05*player,f'Employee {player}: ({x_coord},{y_coord})     Distance to task = {len(dist)}')
-            #fig.text(0.5,0.25-0.05*player,f'Distance to task: ',len(astar(self.floorplan, self.agent_dict['player'+str(player)].coords, self.task_dict['task1'].coords)))
-        #x_coord_adv = self.agent_dict['player'+str(self.num_agents)].x
-        #y_coord_adv = self.agent_dict['player'+str(self.num_agents)].y
-        (x_task, y_task) = self.task_dict['task1'].coords
-        fig.text(0.3,0.05,f'Task coords: ({x_task},{y_task})')
+        #for employee_int in range(self.num_employees):
+        #    x_coord = self.employee_dict[self.db_employees['AgentID'][employee_int]].coords[0]
+        #    y_coord = self.employee_dict[self.db_employees['AgentID'][employee_int]].coords[1]
+            #dist = astar(self.floorplan, self.employee_dict['player'+str(player)].coords, self.task_dict['task1'].coords)
+            #fig.text(0.3,0.3-0.05*player,f'Employee {player}: ({x_coord},{y_coord})     Distance to task = {len(dist)}')
+            #fig.text(0.5,0.25-0.05*player,f'Distance to task: ',len(astar(self.floorplan, self.employee_dict['player'+str(player)].coords, self.task_dict['task1'].coords)))
+       
+        #(x_task, y_task) = self.task_dict['task1'].coords
+        #fig.text(0.3,0.05,f'Task coords: ({x_task},{y_task})')
         #fig.text(0.3,0.10,f'Targets: ({self.target1_x},{self.target1_y}) and ({self.target2_x},{self.target2_y})')
-        #fig.text(0.55,0.25, f'Iteration: {iter}')
-        #plt.savefig(f'saved_images/out_{iter}.png')
-        plt.savefig(fname='./renders/live_render'+str(iter))
+        fig.text(0.55,0.25, f'Iteration: {iter}')
+        #plt.show()
+        plt.savefig(fname='./renders/live_render'+str(iter),dpi=300)
+        fig,ax = render_mpl_table(self.employee_df, header_columns=0, col_width=2.0)
+        fig.savefig("table_mpl.png")
+        plt.close()
 
-    def reset(self):
+    def reset(self):    
 
-        # Player Summoning
-        self.agent_dict = dict()
-        for player_int in range(self.num_agents):
-            self.agent_dict['player'+str(player_int)] = TeamPlayer(self.grid_size)
-        self.agent_dict['player'+str(self.num_agents)] = AdvPlayer(self.grid_size)
-
-        if self.team_wins() or self.adv_wins():
-            self.done_flag = True
-        else:
-            self.done_flag = False
-
-        return self.obs()
-    
-    def staged_reset(self, coords):
-
-        for player_int in range(self.num_agents):
-            self.agent_dict['player'+str(player_int)] = TeamPlayer(self.grid_size)
-            self.agent_dict['player'+str(player_int)].x = coords[2*player_int]
-            self.agent_dict['player'+str(player_int)].y = coords[2*player_int+1]
-        self.agent_dict['player'+str(self.num_agents)] = AdvPlayer(self.grid_size)
-        self.agent_dict['player'+str(self.num_agents)].x = coords[2*self.num_agents]
-        self.agent_dict['player'+str(self.num_agents)].y = coords[2*self.num_agents+1]
-
-        if self.team_wins() or self.adv_wins():
-            self.done_flag = True
-        else:
-            self.done_flag = False
-
+        self.time = 0
+        #self.curr_assignments = {}
         return self.obs()
 
     def obs(self):
-        observation_space = []
-        for player_int in range(self.num_agents):
-            observation_space.append(self.agent_dict['player'+str(player_int)].x)
-            observation_space.append(self.agent_dict['player'+str(player_int)].y)
-        return observation_space # [x,y]
 
-    def print_coords(self):
-        for player in range(self.num_agents):
-            x_coord = self.agent_dict['player'+str(player)].x
-            y_coord = self.agent_dict['player'+str(player)].y
-            print(f'Player {player}: ({x_coord},{y_coord})')
-        print('--------------')
-        #x_coord_adv = self.agent_dict['player'+str(self.num_agents)].x
-        #y_coord_adv = self.agent_dict['player'+str(self.num_agents)].y
-        print(f'Adversary: ({x_coord_adv},{y_coord_adv})')
-        print(f'Targets: ({self.target1_x},{self.target1_y}) and ({self.target2_x},{self.target2_y})')
-        print('--------------')
- 
+        self.observation = []
 
-        x=0
-        y=1
-        coords = [self.agent_dict['player'+str(self.num_agents)].x, self.agent_dict['player'+str(self.num_agents)].y]
+        self.observation.append(self.employee_dict['player'+str(0)].coords[0])
+        self.observation.append(self.employee_dict['player'+str(0)].coords[1])
+        self.observation.append(self.task_dict['task1'].coords[0])
+        self.observation.append(self.task_dict['task1'].coords[1])
 
-        if coords[x] == self.target1_x and coords[y] == self.target1_y:
-            return True
-        if coords[x] == self.target2_x and coords[y] == self.target2_y:
-            return True
+        return self.observation
+
+    def save_video(self):
+
+        save_size = 0
+        img_array = []
+        for i in range(25):
+            filename = './renders/live_render'+str(i)+'.png'
+            img = cv2.imread(filename)
+            height, width, layers = img.shape
+            size = (width,height)
+            save_size = size
+            img_array.append(img)
         
-        return False
+        
+        out = cv2.VideoWriter('project.avi',cv2.VideoWriter_fourcc(*'DIVX'), 2, save_size)
+        
+        for i in range(len(img_array)):
+            out.write(img_array[i])
+        out.release()
