0940a12836c1a11aa32084e2687ea9ffe54c6538
SS8295 <stelios.stavroulakis1@zebra.com>
2022-07-29 20:33:17 -0700

    environment with task manager - reward is total distance traversed

---
+++ draft.py
@@ -0,0 +1,8 @@
+# Observation and Action Space
+#low = np.zeros(4*(self.num_employees))
+#high = np.full(4*(self.num_employees), self.grid_size)
+
+#self.action_space = spaces.Discrete(5)
+#self.observation_space = spaces.Box(np.float32(low), np.float32(high))
+#print("Observation Space = ", self.observation_space)
+#print("Action Space = ",self.action_space)
